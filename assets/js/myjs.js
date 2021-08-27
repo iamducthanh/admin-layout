a2be4bc8f4b5beee912e7668f3451df8dfd6722b
iamducthanh <ducthanh260801@gmail.com>
2021-08-27 21:08:45 +0700

    2

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -8,5 +8,30 @@ function editMenu(){
         left.style.display = 'none';
         right.className = 'col-lg-12';
     }
-    
 }
+let tabAddHH = document.getElementsByClassName("tabAddHH");
+let headAddHH = document.getElementsByClassName("menuTab");
+console.log(tabAddHH);
+for(let i = 1;i<tabAddHH.length;i++){
+    tabAddHH[i].style.display = 'none';
+}
+function onTabAddHH(index){
+    for(let i = 0;i<tabAddHH.length;i++){
+        if(i==index){
+            headAddHH[i].style.borderBottom = '2px solid #564ab1'
+            tabAddHH[i].style.display = 'unset';
+        } else {
+            headAddHH[i].style.borderBottom = '0px'
+            tabAddHH[i].style.display = 'none';
+        }
+    }
+}
+
+function closeModalAddHH(){
+    document.getElementById('modalAddHH').style.display = 'none'
+}
+function onModalAddHH(){
+    document.getElementById('modalAddHH').style.display = 'unset'
+}
+document.getElementById("leftHH").style.display = 'none'
+document.getElementById("rightHH").className = 'col-lg-12';
