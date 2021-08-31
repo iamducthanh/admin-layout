51ce8384c7b1e2f7bc55f2b7b7832d6977a8694d
iamducthanh <ducthanh260801@gmail.com>
2021-08-31 11:14:33 +0700

    edit ảnh

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -27,6 +27,20 @@ function onTabAddHH(index){
     }
 }
 
+const toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+});
+
+function uuid() {
+    var temp_url = URL.createObjectURL(new Blob());
+    var uuid = temp_url.toString();
+    URL.revokeObjectURL(temp_url);
+    return uuid.substr(uuid.lastIndexOf('/') + 1);
+}
+
 function closeModalAddHH(){
     document.getElementById('modalAddHH').style.display = 'none'
 }
